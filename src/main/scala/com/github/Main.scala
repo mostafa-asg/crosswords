70c5172c959ce4fafdafaf13ba4cb21443a47464
mostafa-asg <mostafa.asg@gmail.com>
2018-07-08 22:17:42 +0430

    Add code for main routine

--- src/main/scala/com/github/Main.scala
+++ src/main/scala/com/github/Main.scala
@@ -1,10 +1,21 @@
 package com.github
 
+import scala.io.StdIn
+
 object Main {
 
   def main(args: Array[String]): Unit = {
+    val crosswordsStr = new StringBuilder
 
+    Range(1,11).foreach{ iteration =>
+      crosswordsStr.append(StdIn.readLine())
+      crosswordsStr.append("\n")
+    }
+    val words = StdIn.readLine()
+    val crosswords = CrosswordsBuilder.buildFrom(crosswordsStr.toString(), words.split(";").toSet)
+    val solution = crosswords.solveAllPossibilities.head.stringRepresentation
 
+    println(solution)
   }
 
 }
